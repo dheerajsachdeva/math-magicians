9c9ba3ead1799955210d390a65aea38ddc1592e5
Dheeraj Sachdeva <dheerajarya@gmail.com>
2023-03-29 18:38:15 +0530

    Update operate.test.js

--- src/tests/operate.test.js
+++ src/tests/operate.test.js
@@ -1,114 +1,114 @@
-import operate from "../logic/operate";
+import operate from '../logic/operate';
 
-describe("Check the result of operate", () => {
-  test("5 + 5 = 10", () => {
+describe('Check the result of operate', () => {
+  test('5 + 5 = 10', () => {
     const object = {
       numberOne: 5,
       numberTwo: 5,
-      operation: "+",
+      operation: '+',
     };
     const result = operate(
       object.numberOne,
       object.numberTwo,
-      object.operation
+      object.operation,
     );
-    expect(result).toBe("10");
+    expect(result).toBe('10');
   });
 
-  test("5 * 5 = 25", () => {
+  test('5 * 5 = 25', () => {
     const object = {
       numberOne: 5,
       numberTwo: 5,
-      operation: "x",
+      operation: 'x',
     };
     const result = operate(
       object.numberOne,
       object.numberTwo,
-      object.operation
+      object.operation,
     );
-    expect(result).toBe("25");
+    expect(result).toBe('25');
   });
 
-  test("5 % 2 = 1", () => {
+  test('5 % 2 = 1', () => {
     const object = {
       numberOne: 5,
       numberTwo: 2,
-      operation: "%",
+      operation: '%',
     };
     const result = operate(
       object.numberOne,
       object.numberTwo,
-      object.operation
+      object.operation,
     );
-    expect(result).toBe("1");
+    expect(result).toBe('1');
   });
 
-  test("5 - 5 = 0", () => {
+  test('5 - 5 = 0', () => {
     const object = {
       numberOne: 5,
       numberTwo: 5,
-      operation: "-",
+      operation: '-',
     };
     const result = operate(
       object.numberOne,
       object.numberTwo,
-      object.operation
+      object.operation,
     );
-    expect(result).toBe("0");
+    expect(result).toBe('0');
   });
-  test("10 ÷ 5 = 2", () => {
+  test('10 ÷ 5 = 2', () => {
     const object = {
       numberOne: 10,
       numberTwo: 5,
-      operation: "÷",
+      operation: '÷',
     };
     const result = operate(
       object.numberOne,
       object.numberTwo,
-      object.operation
+      object.operation,
     );
-    expect(result).toBe("2");
+    expect(result).toBe('2');
   });
 
-  test("10 - 5 = 5", () => {
+  test('10 - 5 = 5', () => {
     const object = {
       numberOne: 10,
       numberTwo: 5,
-      operation: "-",
+      operation: '-',
     };
     const result = operate(
       object.numberOne,
       object.numberTwo,
-      object.operation
+      object.operation,
     );
-    expect(result).toBe("5");
+    expect(result).toBe('5');
   });
 
-  test("25 + 5 = 30", () => {
+  test('25 + 5 = 30', () => {
     const object = {
       numberOne: 25,
       numberTwo: 5,
-      operation: "+",
+      operation: '+',
     };
     const result = operate(
       object.numberOne,
       object.numberTwo,
-      object.operation
+      object.operation,
     );
-    expect(result).toBe("30");
+    expect(result).toBe('30');
   });
 
-  test("10 % 3 = 1", () => {
+  test('10 % 3 = 1', () => {
     const object = {
       numberOne: 10,
       numberTwo: 3,
-      operation: "%",
+      operation: '%',
     };
     const result = operate(
       object.numberOne,
       object.numberTwo,
-      object.operation
+      object.operation,
     );
-    expect(result).toBe("1");
+    expect(result).toBe('1');
   });
 });
